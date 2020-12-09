3938a20bf5cc098824aeeceac900c556b41c290c
young <>
2020-12-09 20:42:05 +0100

    setup tinymce + blog.php

---
+++ blog.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+     require 'common/header.php';
+    ?>
+  </head>
+  <body>
+     <textarea class = "tinymce"></textarea>
+     <!-- js -->
+     <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+     <script type="text/javascript" src="plugins/tinymce/tinymce.min.js"></script>
+     <script type="text/javascript" src="plugins/tinymce/init-tinymce.js"></script>
+   </body>
+</html>
+<!---
+ TODO (?) -aggiungere bottone "POST"
+            + domanda : quando aggiungi un nuovo post crei una nuova pagina?
+            + domanda : vetrina dei post nel blog con riferimento a un DB?
+          -->
