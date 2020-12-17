1ee28fd3b5450c6d5090044265bc975ab60b6c18
ema-aka-young <youngbastardino@hotmail.com>
2020-12-17 22:17:25 +0100

    pagina di benvenuto con immagine [DA FINIRE]

---
+++ welcome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>FilmSearch - Welcome</title>
+</head>
+<?php
+   require('common/header.php');
+
+ ?>
+<body>
+<img src="/images/welcome.png" alt="Welcome Page" align="center" width="450" height="450">
+
+<?php
+echo "<h1>";
+      echo "Hello ";
+      echo "$nome " . "$cogn ";
+      echo "your data have been registered.";
+  echo "</h1>\n";
+?>
+
+</body>
